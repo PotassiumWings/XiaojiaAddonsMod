8b078c8566cb33e4193e0d3d6e5e66b72872319c
PotassiumWings <mumuyuyululu@126.com>
2021-12-03 08:48:26 +0800

    optimize, change from draw to opengui

--- src/main/java/com/xiaojia/xiaojiaaddons/Features/Dungeons/AutoCloseSecretChest.java
+++ src/main/java/com/xiaojia/xiaojiaaddons/Features/Dungeons/AutoCloseSecretChest.java
@@ -5,6 +5,7 @@ import com.xiaojia.xiaojiaaddons.Objects.Checker;
 import com.xiaojia.xiaojiaaddons.utils.SkyblockUtils;
 import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.inventory.ContainerChest;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -12,7 +13,7 @@ import static com.xiaojia.xiaojiaaddons.utils.MinecraftUtils.getPlayer;
 
 public class AutoCloseSecretChest {
     @SubscribeEvent
-    public void onGuiDraw(GuiScreenEvent.DrawScreenEvent event) {
+    public void onGuiDraw(GuiOpenEvent event) {
         if (!Checker.enabled) return;
         if (!Configs.AutoCloseSecretChest || !SkyblockUtils.isInDungeon()) return;
         if (!(event.gui instanceof GuiChest)) return;
