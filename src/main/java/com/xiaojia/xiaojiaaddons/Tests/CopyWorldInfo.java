db00ced14fb8fa1482010aa22f5a3dd83eeb1ecd
PotassiumWings <mumuyuyululu@126.com>
2022-04-16 22:11:27 +0800

    2.4.5.6: coords gb change, xj cp optimize

--- src/main/java/com/xiaojia/xiaojiaaddons/Tests/CopyWorldInfo.java
+++ src/main/java/com/xiaojia/xiaojiaaddons/Tests/CopyWorldInfo.java
@@ -1,17 +1,13 @@
 package com.xiaojia.xiaojiaaddons.Tests;
 
-import com.xiaojia.xiaojiaaddons.XiaojiaAddons;
 import com.xiaojia.xiaojiaaddons.utils.BlockUtils;
 import com.xiaojia.xiaojiaaddons.utils.ChatLib;
-import com.xiaojia.xiaojiaaddons.utils.CommandsUtils;
 import net.minecraft.block.Block;
-import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
 
 import java.util.ArrayList;
-
-import static com.xiaojia.xiaojiaaddons.utils.MinecraftUtils.getWorld;
 
 public class CopyWorldInfo {
     public static int x, y, z;
@@ -42,12 +38,14 @@ public class CopyWorldInfo {
     public static void paste() {
         new Thread(() -> {
             int index = 0;
+            ChatLib.say(String.format("/fill %d %d %d %d %d %d air", x, y, z, x + dx, y + dy, z + dz));
             for (int i = x; i <= x + dx; i++) {
                 for (int j = y; j <= y + dy; j++) {
                     for (int k = z; k <= z + dz; k++) {
-                        ChatLib.say(String.format("/setblock %d %d %d %s %d", i, j, k,
-                                Block.blockRegistry.getNameForObject(blockStates.get(index).getBlock()),
-                                blockStates.get(index).getBlock().getMetaFromState(blockStates.get(index))));
+                        if (blockStates.get(index).getBlock() != Blocks.air)
+                            ChatLib.say(String.format("/setblock %d %d %d %s %d", i, j, k,
+                                    Block.blockRegistry.getNameForObject(blockStates.get(index).getBlock()),
+                                    blockStates.get(index).getBlock().getMetaFromState(blockStates.get(index))));
 //                        XiaojiaAddons.mc.thePlayer.getEntityWorld().setBlockState(new BlockPos(i, j, k), blockStates.get(index));
                         index++;
                     }
