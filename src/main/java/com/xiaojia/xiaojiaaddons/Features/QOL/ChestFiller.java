94d56e9a4a7336973ff774c241c6168020d16b89
trytodupe <liuandi@live.com>
2022-05-24 08:09:27 +0800

    more fixes

--- src/main/java/com/xiaojia/xiaojiaaddons/Features/QOL/ChestFiller.java
+++ src/main/java/com/xiaojia/xiaojiaaddons/Features/QOL/ChestFiller.java
@@ -20,19 +20,20 @@ import static com.xiaojia.xiaojiaaddons.utils.MinecraftUtils.getPlayer;
 public class ChestFiller {
     private static boolean enabled = false, done = false;
 
+    private Thread pushingThread = null;
     private static int lastId = -1;
 
     private static void clear() {
+        if (enabled) ChatLib.chat("Chest Filler &cdeactivated");
         lastId = -1;
         enabled = false;
         done = false;
-        ChatLib.chat("Chest Filler &cdeactivated");
     }
 
     public static void toggle() {
-        enabled = !enabled;
-        if (enabled) ChatLib.chat("Chest Filler &aactivated");
+        if (!enabled) ChatLib.chat("Chest Filler &aactivated");
         else clear();
+        enabled = !enabled;
     }
 
     public void click(Inventory inventory, int slot, int mode, int button, int incrementWindowId) {
@@ -59,41 +60,56 @@ public class ChestFiller {
         }
         if (enabled && !done && inventory.getName().equals("Large Chest")) {
             done = true;
-            new Thread(() -> {
-                try {
-                    Thread.sleep(500);
-                    if (ControlUtils.getOpenedInventory() == null || !inventory.getName().equals("Large Chest")) return;
-                    int i, amount;
-                    boolean isEmpty = true;
-                    HashSet<Slot> emptySlots = new HashSet<>();
-                    for (i = 0; i < 54; i++) {
-                        if (inventory.getItemInSlot(i) == null) emptySlots.add(inventory.getSlots().get(i));
-                    }
-                    //inv slots: 54-89
-                    for (i = 54; i < 90; i++) {
-                        if (inventory.getItemInSlot(i) == null) continue;
-                        amount = inventory.getItemInSlot(i).stackSize;
-                        if (inventory.getItemInSlot(i).getItem().equals(Item.getByNameOrId("minecraft:prismarine_crystals"))
-                        && amount >= emptySlots.size()) {
-                            isEmpty = false;
-                            break;
+            if (pushingThread == null || !pushingThread.isAlive()) {
+                pushingThread = new Thread(() -> {
+                    try {
+                        Thread.sleep(200);
+                        if (ControlUtils.getOpenedInventory() == null || !inventory.getName().equals("Large Chest"))
+                            return;
+                        int i, amount;
+                        boolean isEmpty = true;
+                        HashSet<Slot> emptySlots = new HashSet<>();
+                        for (i = 0; i < 54; i++) {
+                            if (inventory.getItemInSlot(i) == null) emptySlots.add(inventory.getSlots().get(i));
                         }
+                        //inv slots: 54-89
+                        for (i = 54; i < 90; i++) {
+                            if (inventory.getItemInSlot(i) == null) continue;
+                            amount = inventory.getItemInSlot(i).stackSize;
+                            if (inventory.getItemInSlot(i).getItem().equals(Item.getByNameOrId("minecraft:prismarine_crystals"))
+                                    && amount >= emptySlots.size()) {
+                                isEmpty = false;
+                                break;
+                            }
+                        }
+                        if (emptySlots.isEmpty()) return;
+                        if (!isEmpty) {
+                            click(inventory, i, 0, 0, 0);
+                            Thread.sleep(200);
+                            click(inventory, -999, 5, 4, 0);
+                            for (Slot slot1 : emptySlots) click(inventory, slot1.slotNumber, 5, 5, 0);
+                            click(inventory, -999, 5, 6, 0);
+                            Thread.sleep(100);
+                            click(inventory, i, 0, 0, 0);
+                            Thread.sleep(100);
+                            getPlayer().closeScreen();
+                        } else {
+                            ChatLib.chat("Please stack your items.");
+                            clear();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                    if (!isEmpty) {
-                        click(inventory, i, 0, 0, 0);
-                        click(inventory, -999, 5, 4, 0);
-                        for (Slot slot1 : emptySlots) click(inventory, slot1.slotNumber, 5, 5, 0);
-                        click(inventory, -999, 5, 6, 0);
-                        Thread.sleep(100);
-                        click(inventory, i, 0, 0, 0);
-                    } else {
-                        ChatLib.chat("Please stack your items.");
-                        clear();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }).start();
+                });
+                pushingThread.start();
+            }
         }
+    }
+
+    @SubscribeEvent
+    public void onTickMove(TickEndEvent event) {
+        Inventory inventory = ControlUtils.getOpenedInventory();
+        if ((ControlUtils.getOpenedInventory() == null || !inventory.getName().equals("Large Chest"))
+                && (pushingThread != null)) pushingThread.interrupt();
     }
 }
