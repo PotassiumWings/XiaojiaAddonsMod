c5338879fae281f91ef5dc43f5cf8bc8439cbbd8
PotassiumWings <mumuyuyululu@126.com>
2022-06-16 02:12:40 +0800

    2.4.7.2: auto meat fix, auto question add

--- src/main/java/com/xiaojia/xiaojiaaddons/Features/Accentry/AutoMeat.java
+++ src/main/java/com/xiaojia/xiaojiaaddons/Features/Accentry/AutoMeat.java
@@ -7,16 +7,32 @@ import com.xiaojia.xiaojiaaddons.Objects.Inventory;
 import com.xiaojia.xiaojiaaddons.XiaojiaAddons;
 import com.xiaojia.xiaojiaaddons.utils.ChatLib;
 import com.xiaojia.xiaojiaaddons.utils.ControlUtils;
+import net.minecraft.entity.IMerchant;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.ContainerMerchant;
+import net.minecraft.inventory.InventoryMerchant;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.village.MerchantRecipe;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.lang.reflect.Field;
 
 public class AutoMeat {
     private Thread sellThread = null;
+    private static Field merchantField = null;
+    static {
+        try {
+            merchantField = ContainerMerchant.class.getDeclaredField("field_75178_e");
+        } catch (NoSuchFieldException e) {
+            try {
+                merchantField = ContainerMerchant.class.getDeclaredField("theMerchant");
+            } catch (NoSuchFieldException e1) {
+                e1.printStackTrace();
+            }
+        }
+        merchantField.setAccessible(true);
+    }
 
     @SubscribeEvent
     public void onTick(TickEndEvent event) {
@@ -26,12 +42,12 @@ public class AutoMeat {
                     for (int i = 3; i < 39; i++) {
                         if (!should()) return;
                         Inventory inventory = ControlUtils.getOpenedInventory();
+                        if (inventory == null) return;
                         ItemStack itemStack = inventory.getItemInSlot(i);
                         if (itemStack == null || !itemStack.getDisplayName().contains("精盐肉块") ||
                                 itemStack.stackSize != 64) continue;
                         inventory.click(i, false, "LEFT", 0);
                         inventory.click(0, false, "LEFT", 0);
-                        inventory.click(2, true, "LEFT", 0);
                         Thread.sleep(100);
                     }
                 } catch (Exception e) {
@@ -46,13 +62,17 @@ public class AutoMeat {
         if (!Checker.enabled || !Configs.AutoMeat) return false;
         Inventory inventory = ControlUtils.getOpenedInventory();
         if (inventory == null) return false;
-        if (XiaojiaAddons.isDebug()) ChatLib.chat(inventory.container.getClass() + ", " + inventory.getName());
+        // recipe filled
+        if (inventory.getItemInSlot(2) != null) return false;
+
         if (!(inventory.container instanceof ContainerMerchant)) return false;
-        MerchantRecipe recipe = ((ContainerMerchant) inventory.container).getMerchantInventory().getCurrentRecipe();
-        if (recipe == null) return false;
-        ItemStack itemStack = recipe.getItemToSell();
-        ItemStack first = recipe.getItemToBuy();
-        ItemStack second = recipe.getSecondItemToBuy();
-        return itemStack.getItem() == Item.getItemFromBlock(Blocks.beacon) && first.getItem() == Items.porkchop;
+
+        ContainerMerchant merchant = ((ContainerMerchant) inventory.container);
+        String name = "";
+        try {
+            name = (((IMerchant) merchantField.get(merchant)).getDisplayName().getFormattedText());
+        } catch (IllegalAccessException ignored) {
+        }
+        return name.contains("精盐贤者");
     }
 }
