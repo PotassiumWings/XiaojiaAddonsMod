8c29aba2f3f714d75debe1b522f672486ce688f6
PotassiumWings <mumuyuyululu@126.com>
2022-06-24 16:19:13 +0800

    2.4.8: version 2.4.8

--- src/main/java/com/xiaojia/xiaojiaaddons/XiaojiaAddons.java
+++ src/main/java/com/xiaojia/xiaojiaaddons/XiaojiaAddons.java
@@ -179,7 +179,7 @@ import java.util.ArrayList;
 @Mod(modid = XiaojiaAddons.MODID, version = XiaojiaAddons.VERSION)
 public class XiaojiaAddons {
     public static final String MODID = "xiaojiaaddons";
-    public static final String VERSION = "2.4.7.2";
+    public static final String VERSION = "2.4.8";
     public static final Minecraft mc = Minecraft.getMinecraft();
     public static ArrayList<Setting> settings = Config.collect(Configs.class);
     public static GuiScreen guiToOpen = null;
