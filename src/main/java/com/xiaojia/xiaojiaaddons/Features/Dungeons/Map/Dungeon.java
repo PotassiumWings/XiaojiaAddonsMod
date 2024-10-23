800425a136f2d7a26e320030171013368cbb129f
PotassiumWings <mumuyuyululu@126.com>
2024-10-23 13:50:48 +0800

    livid message

--- src/main/java/com/xiaojia/xiaojiaaddons/Features/Dungeons/Map/Dungeon.java
+++ src/main/java/com/xiaojia/xiaojiaaddons/Features/Dungeons/Map/Dungeon.java
@@ -63,7 +63,8 @@ public class Dungeon {
             "[BOSS] Scarf: This is where the journey ends for you, Adventurers.",
             "[BOSS] The Professor: I was burdened with terrible news recently...",
             "[BOSS] Thorn: Welcome Adventurers! I am Thorn, the Spirit! And host of the Vegan Trials!",
-            "[BOSS] Livid: Welcome, you arrive right on time. I am Livid, the Master of Shadows.",
+//            "[BOSS] Livid: Welcome, you arrive right on time. I am Livid, the Master of Shadows.",
+            "[BOSS] Livid: Welcome, you've arrived right on time. I am Livid, the Master of Shadows.",
             "[BOSS] Sadan: So you made it all the way here... Now you wish to defy me? Sadan?!",
             "[BOSS] Necron: Finally, I heard so much about you. The Eye likes you very much.",
             "[BOSS] Maxor: I’VE BEEN TOLD I COULD HAVE A BIT OF FUN WITH YOU."
